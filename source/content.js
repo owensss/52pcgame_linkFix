4c0e600f88723466914bf0c5f3f780942625b12c
owensss <ownesss@gmail.com>
2013-06-28 01:40:17 +0800

    add forum replacer & home page replace

--- source/content.js
+++ source/content.js
@@ -1,14 +1,20 @@
-// window.onload = gao;
-
-function replacer(match, p1, offset, string) {
-	return "http://121.199.23.180/bbs/forum.php?mod=viewthread&tid="+p1;
-}
-
-var pattern=/http:\/\/bbs\.52pcgame\.com\/thread\-([0-9]+)(.*)\.html/;
+var thread_pattern=/http:\/\/bbs\.52pcgame\.com\/thread\-([0-9]+)(.*)\.html/;
+var forum_pattern=/http:\/\/bbs\.52pcgame\.com\/forum-([0-9]+)(.*).html/;
 
 var links = document.links;
 for (var i = 0; i < links.length; ++i) {
-	// console.log(links[i]);
-	links[i].href = links[i].href.replace(pattern, replacer);
+	// replace mainpage
+	links[i].href = links[i].href.replace("\"http://bbs.52pcgame.com\"", "\"http://121.199.23.180\"");
+	// replace thread
+	links[i].href = links[i].href.replace(thread_pattern, function(match, p1, offset, string) {
+		return "http://121.199.23.180/bbs/forum.php?mod=viewthread&tid="+p1;
+	}
+	);
+	// replace forum
+	links[i].href = links[i].href.replace(forum_pattern, function(match, p1) {
+		return "http://121.199.23.180/bbs/forum.php?mod=forumdisplay&tid="+p1;
+	}
+	);
+
 }
 
